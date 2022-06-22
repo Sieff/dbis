dedb5226146f6e1c7959261d68e0a87103212776
Sieff <stargamehd3d>
2022-06-22 17:50:41 +0200

    Blatt 6 Abgabe

--- Blatt6_2/src/CrossTable.java
+++ Blatt6_2/src/CrossTable.java
@@ -1,7 +1,11 @@
-enum ShopGran {
+import java.util.ArrayList;
+import java.util.List;
+
+enum BranchGran {
     shop {
         @Override
-        public ShopGran previous() {
+        public BranchGran previous() {
+            System.out.println("Already lowest granulate!");
             return this;
         }
     },
@@ -9,15 +13,16 @@ enum ShopGran {
     region,
     country {
         @Override
-        public ShopGran next() {
+        public BranchGran next() {
+            System.out.println("Already highest granulate!");
             return this;
         }
     };
 
-    public ShopGran next() {
+    public BranchGran next() {
         return values()[ordinal() + 1];
     }
-    public ShopGran previous() {
+    public BranchGran previous() {
         return values()[ordinal() - 1];
     }
 }
@@ -26,14 +31,16 @@ enum ProductGran {
     article {
         @Override
         public ProductGran previous() {
+            System.out.println("Already lowest granulate!");
             return this;
         }
     },
-    productGroup,
-    productFamily,
-    productCategory {
+    product_group,
+    product_family,
+    product_category {
         @Override
         public ProductGran next() {
+            System.out.println("Already highest granulate!");
             return this;
         }
     };
@@ -50,6 +57,7 @@ enum TimeGran {
     date {
         @Override
         public TimeGran previous() {
+            System.out.println("Already lowest granulate!");
             return this;
         }
     },
@@ -59,6 +67,7 @@ enum TimeGran {
     year {
         @Override
         public TimeGran next() {
+            System.out.println("Already highest granulate!");
             return this;
         }
     };
@@ -77,14 +86,19 @@ enum Grans {
 
 public class CrossTable {
 
-    private ShopGran shopGran = ShopGran.shop;
+    private BranchGran branchGran = BranchGran.shop;
     private ProductGran productGran = ProductGran.article;
     private TimeGran timeGran = TimeGran.day;
+    private List<Cuboid> cuboidList = new ArrayList<>();
 
+    private boolean aggregateBranch = false;
+    private boolean aggregateProduct = false;
+    private boolean aggregateTime = false;
 
-    public CrossTable() {
-        System.out.println(shopGran.previous());
-    }
+    private int lineLength = 15;
+    private int numberOfRows;
+    private int currentPage;
+
 
     public void drillUp(Grans granToDrill) {
         switch (granToDrill) {
@@ -92,7 +106,7 @@ public class CrossTable {
                 productGran.next();
                 break;
             case Shop:
-                shopGran.next();
+                branchGran.next();
                 break;
             case time:
                 timeGran.next();
@@ -106,7 +120,7 @@ public class CrossTable {
                 productGran.previous();
                 break;
             case Shop:
-                shopGran.previous();
+                branchGran.previous();
                 break;
             case time:
                 timeGran.previous();
@@ -114,4 +128,153 @@ public class CrossTable {
         }
     }
 
+    public void nextPage() {
+        if (currentPage + 1 < (countValidCuboids() - 1) / numberOfRows) {
+            this.currentPage++;
+            printData();
+        } else {
+            System.out.println("Already on last page5!");
+        }
+
+    }
+
+    private int countValidCuboids() {
+        int i = 0;
+        for (Cuboid cuboid: cuboidList) {
+            if (isValid(cuboid)) {
+                i++;
+            }
+        }
+        return i;
+    }
+
+    public void previousPage() {
+        if (currentPage == 0) {
+            System.out.println("Already on first page!");
+        } else {
+            this.currentPage--;
+            printData();
+        }
+    }
+
+    public void printData() {
+        printFrame();
+        System.out.format("| %" + lineLength * 2 + "s | %" + lineLength * 2 + "s | %" + lineLength + "s | %" + lineLength + "s |\n", branchGran, productGran, timeGran, "Total");
+        printFrame();
+        print();
+        printFrame();
+    }
+
+    private void printFrame() {
+        System.out.print("| ");
+        for (int i = 0; i < 6 * lineLength + (3 * 3); i++) {
+            System.out.print("-");
+        }
+        System.out.println(" |");
+    }
+
+    private void print() {
+        int numberOfValidEntries = 0;
+        int entriesPrinted = 0;
+        int currentEntryIndex = 0;
+        while (entriesPrinted < numberOfRows) {
+            if (currentEntryIndex == cuboidList.size()) {
+                //System.out.format("%" + 6 * lineLength + (3 * 3) + "s\n", "Reached End of Data!");
+                break;
+            }
+            Cuboid cuboid = cuboidList.get(currentEntryIndex);
+            boolean valid = isValid(cuboid);
+            if (valid) {
+                if (numberOfValidEntries >= currentPage * numberOfRows && numberOfValidEntries < (currentPage + 1) * numberOfRows) {
+                    cuboid.print(lineLength);
+                    entriesPrinted++;
+                }
+                currentEntryIndex++;
+                numberOfValidEntries++;
+            } else {
+                currentEntryIndex++;
+            }
+        }
+    }
+
+    private boolean isValid(Cuboid cuboid) {
+        if (isAggregateBranch() != (cuboid.getBranchLabel() == null)) {
+            return false;
+        }
+        if (isAggregateProduct() != (cuboid.getProductLabel() == null)) {
+            return false;
+        }
+        if (isAggregateTime() != (cuboid.getTimeLabel() == null)) {
+            return false;
+        }
+        return true;
+    }
+
+    public boolean isAggregateBranch() {
+        return aggregateBranch;
+    }
+
+    public void setAggregateBranch(boolean aggregateBranch) {
+        this.aggregateBranch = aggregateBranch;
+    }
+
+    public boolean isAggregateProduct() {
+        return aggregateProduct;
+    }
+
+    public void setAggregateProduct(boolean aggregateProduct) {
+        this.aggregateProduct = aggregateProduct;
+    }
+
+    public boolean isAggregateTime() {
+        return aggregateTime;
+    }
+
+    public void setAggregateTime(boolean aggregateTime) {
+        this.aggregateTime = aggregateTime;
+    }
+
+    public void setCurrentPage(int currentPage) {
+        this.currentPage = currentPage;
+    }
+
+    public void add(Cuboid cuboid) {
+        cuboidList.add(cuboid);
+    }
+
+    public void clearData() {
+        cuboidList.clear();
+    }
+
+    public void setLineLength(int lineLength) {
+        this.lineLength = lineLength;
+    }
+
+    public void setBranchGran(BranchGran branchGran) {
+        this.branchGran = branchGran;
+    }
+
+    public BranchGran getBranchGran() {
+        return branchGran;
+    }
+
+    public void setProductGran(ProductGran productGran) {
+        this.productGran = productGran;
+    }
+
+    public ProductGran getProductGran() {
+        return productGran;
+    }
+
+    public void setTimeGran(TimeGran timeGran) {
+        this.timeGran = timeGran;
+    }
+
+    public TimeGran getTimeGran() {
+        return timeGran;
+    }
+
+    public void setNumberOfRows(int numberOfRows) {
+        this.numberOfRows = numberOfRows;
+    }
 }
